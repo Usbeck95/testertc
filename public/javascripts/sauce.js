1b54f11a7640ec57ed389988b709748efa7e5ffa
Carina <carinausbeck@live.dk>
2022-01-25 21:26:11 +0100

    pug driller

--- public/javascripts/sauce.js
+++ public/javascripts/sauce.js
@@ -1,1 +1,13 @@
-console.log("helloworlds");
+
+//const apikey = "1bkG7ky5cmw5SLyvNfElcR1iYVzs38Zq";
+let btn = document.getElementById("btnSearch");
+if (btn) {
+btn.addEventListener("click", console.log("helloworlds"));
+}
+function goApiGo() {
+let url = "https://api.giphy.com/v1/stickers/search?&limit=10&rating=g&api_key=1bkG7ky5cmw5SLyvNfElcR1iYVzs38Zq&q=";
+let search = document.getElementById("search").value;
+console.log(search);
+url = url.concat(search);
+console.log(url);
+}
